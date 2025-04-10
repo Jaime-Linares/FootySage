d5b38bc06c7530a8d80a0a5b14028b28fd989b63
Jaime-Linares <jailinbar@alum.us.es>
2025-04-10 20:34:50 +0200

    feat: landing page first part

--- frontend/src/views/LandingPage/LandingPage.jsx
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import CustomButton from '../../components/CustomButton';
+import landingIllustration from '../../assets/images/landing_illustration.png';
+import './styles/LandingPage.css';
 
 
 const LandingPage = () => {
     return (
-        <div>
-          <h1>ESTO ES LA LANDING PAGE</h1>
-        </div>
+        <section className="hero-section">
+            <div className="hero-content">
+                <div className="hero-text">
+                    <h1>FootySage, el sabio del fútbol</h1>
+                    <p>
+                        Analiza competiciones y partidos, simula encuentros en tiempo real y
+                        predice el equipo ganador como nunca antes lo habías visto con IA.
+                    </p>
+                    <p>
+                        Domina el juego con análisis visuales, datos en tiempo real y estadísticas avanzadas.
+                    </p>
+                    <CustomButton
+                        title="Ver demo"
+                        onPress={() => console.log('Demo clicked')}
+                        buttonStyle={{ marginTop: '22px', fontSize: '20px', padding: '13px 30px', borderRadius: '40px' }}
+                        textStyle={{ fontSize: '20px' }}
+                    />
+                </div>
+                <div className="hero-image">
+                  <img src={landingIllustration} alt="IA fútbol análisis" />
+                </div>
+            </div>
+        </section>
     );
 };
 
