e4dba04564d2165f7dc9095b1d123875471dbcdf
Jaime-Linares <jailinbar@alum.us.es>
2025-04-11 11:31:38 +0200

    feat: add models to allow admin all operations

--- backend/matches/admin.py
+++ backend/matches/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Team, Competition, Match, Event, UserFavoriteTeam, UserFavoriteMatch, UserTimesAnalyzedMatch
 
-# Register your models here.
+
+
+admin.site.register(Team)
+admin.site.register(Competition)
+admin.site.register(Match)
+admin.site.register(Event)
+admin.site.register(UserFavoriteTeam)
+admin.site.register(UserFavoriteMatch)
+admin.site.register(UserTimesAnalyzedMatch)
