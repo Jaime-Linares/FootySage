0aa11b0b4f6a1f75b2a90f69d96929490a55b758
Jaime-Linares <jailinbar@alum.us.es>
2025-04-12 19:55:47 +0200

    feat: add register endpoint

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 class User(AbstractUser):
     avatar_url = models.URLField(blank=True, null=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
 
     def __str__(self):
         return self.username + ", " + self.email + " (" + self.first_name + " " + self.last_name + ")"
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
+from .models import User
+from matches.models import Team, UserFavoriteTeam
+from django.utils.translation import override
 
 
-
+# This serializer is used to authenticate users and obtain JWT tokens
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
@@ -13,4 +17,34 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
         token['email'] = user.email
         return token
+
+
+# This serializer is used to register new users
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    favorite_teams = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all(), many=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name','password', 'password2', 'favorite_teams')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Las contraseñas no coinciden."})
+        favorite_teams = self.initial_data.get('favorite_teams', [])
+        if not favorite_teams:
+            raise serializers.ValidationError({"favorite_teams": "Debes elegir al menos un equipo favorito."})
+        return attrs
+
+    def create(self, validated_data):
+        favorite_teams = validated_data.pop('favorite_teams', [])
+        validated_data.pop('password2')
+
+        user = User.objects.create_user(**validated_data)
+
+        for team in favorite_teams:
+            UserFavoriteTeam.objects.create(user=user, team=team)
+
+        return user
 
