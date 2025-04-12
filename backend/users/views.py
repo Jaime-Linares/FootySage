e1bbc004e90e2a2dd60818ad929dd8c3f093e366
Jaime-Linares <jailinbar@alum.us.es>
2025-04-12 14:52:46 +0200

    fix: add permission method

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,11 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import MyTokenObtainPairSerializer
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsNotAuthenticated
 
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
+    permission_classes = [IsNotAuthenticated]
     serializer_class = MyTokenObtainPairSerializer
 
