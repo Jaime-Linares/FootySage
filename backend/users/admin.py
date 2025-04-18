8ebcf81f1c0664baf7a99de7581a01661e3c71c1
Jaime-Linares <jailinbar@alum.us.es>
2025-04-18 14:22:26 +0200

    feat: allow admin change users' avatar name

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -8,4 +8,15 @@ class UserAdmin(BaseUserAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.fields]
 
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Avatar personalizado', {'fields': ('avatar_name',)}),
+    )
+
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Avatar personalizado', {
+            'classes': ('wide',),
+            'fields': ('avatar_name',),
+        }),
+    )
+
 admin.site.register(User, UserAdmin)
