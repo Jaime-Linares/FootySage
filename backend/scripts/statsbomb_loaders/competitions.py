8e43153292442cf878b9fb3e504e6cd45e34d97d
Jaime-Linares <jailinbar@alum.us.es>
2025-04-20 15:54:44 +0200

    feat: add teams and matches loader

--- backend/scripts/statsbomb_loaders/competitions.py
+++ backend/scripts/statsbomb_loaders/competitions.py
@@ -1,23 +1,22 @@
+# encoding:utf-8
 from statsbombpy import sb
 from matches.models import Competition
 from .utils import get_competition_extra_data
-#from .matches import create_macthes_by_competition_id_and_season_id
+from .matches import create_macthes_by_competition_id_and_season_id
 
 
-def create_competition(competition_id, season_id, gender) -> None:
+def create_competition(competition_id, season_id, gender):
     '''
     Create a competition object in the database.
     params:
-        competition_id: The ID of the competition to create.
-        season_id: The ID of the season to create.
-        gender: The genre of the competition to create.
+        competition_id (int): The ID of the competition to create.
+        season_id (int): The ID of the season to create.
+        gender (str): The genre of the competition to create.
     returns:
         None
     '''
     competitions = sb.competitions()
-    competition = competitions[(competitions['competition_id'] == competition_id) & 
-                                  (competitions['season_id'] == season_id) & 
-                                  (competitions['competition_gender'] == gender)]
+    competition = competitions[(competitions['competition_id'] == competition_id) & (competitions['season_id'] == season_id) & (competitions['competition_gender'] == gender)]
     if competition.shape[0] == 0:
         raise ValueError('Competición no encontrada.')
     competition_data = competition.iloc[0]
@@ -33,7 +32,7 @@ def create_competition(competition_id, season_id, gender) -> None:
         competition_logo_url=competition_logo_url,
     )
     competition.save()
-
-    #create_macthes_by_competition_id_and_season_id
     print(f"Competición {competition_data["competition_name"]} ({competition_data["season_name"]}) creada exitosamente.")
+    
+    create_macthes_by_competition_id_and_season_id(competition_id, season_id)
 
