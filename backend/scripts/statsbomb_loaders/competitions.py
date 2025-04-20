35ce81227a4426560d4ffb83c96c71dcbf2652f5
Jaime-Linares <jailinbar@alum.us.es>
2025-04-20 13:58:16 +0200

    feat: add competition loader

---
+++ backend/scripts/statsbomb_loaders/competitions.py
@@ -0,0 +1,39 @@
+from statsbombpy import sb
+from matches.models import Competition
+from .utils import get_competition_extra_data
+#from .matches import create_macthes_by_competition_id_and_season_id
+
+
+def create_competition(competition_id, season_id, gender) -> None:
+    '''
+    Create a competition object in the database.
+    params:
+        competition_id: The ID of the competition to create.
+        season_id: The ID of the season to create.
+        gender: The genre of the competition to create.
+    returns:
+        None
+    '''
+    competitions = sb.competitions()
+    competition = competitions[(competitions['competition_id'] == competition_id) & 
+                                  (competitions['season_id'] == season_id) & 
+                                  (competitions['competition_gender'] == gender)]
+    if competition.shape[0] == 0:
+        raise ValueError('Competición no encontrada.')
+    competition_data = competition.iloc[0]
+    if Competition.objects.filter(statsbomb_id=competition_data["competition_id"]).exists():
+        print(f"La competición {competition_data["competition_name"]} ({competition_data["season_name"]}) ya existe en la base de datos.")
+        return
+        
+    api_id, competition_logo_url = get_competition_extra_data(competition_id)
+    competition = Competition(
+        statsbomb_id=competition_id,
+        api_id=api_id,
+        name=competition_data["competition_name"],
+        competition_logo_url=competition_logo_url,
+    )
+    competition.save()
+
+    #create_macthes_by_competition_id_and_season_id
+    print(f"Competición {competition_data["competition_name"]} ({competition_data["season_name"]}) creada exitosamente.")
+
