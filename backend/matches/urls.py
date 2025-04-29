b76a3c83af424c52dabf3a56dcd4655511786824
Jaime-Linares <jailinbar@alum.us.es>
2025-04-29 16:45:11 +0200

    feat: add endpoint for obtain match details

--- backend/matches/urls.py
+++ backend/matches/urls.py
@@ -3,6 +3,7 @@ from .views import TeamListView, UpcomingMatchesView, FilteredCompetitionsView, 
 
 
 urlpatterns = [
+    path("match_detail/", MatchDetailView.as_view(), name="match_detail"),
     path("teams/", TeamListView.as_view(), name="team_list"),
     path("upcoming_matches/", UpcomingMatchesView.as_view(), name="upcoming_matches"),
     path("competitions_match/", FilteredCompetitionsView.as_view(), name="filtered_competitions"),
--- backend/matches/views.py
+++ backend/matches/views.py
@@ -9,6 +9,21 @@ import datetime
 import requests
 
 
+# --- View to handle requests for the details of a match ----------------------------------------------------------------------------
+class MatchDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        match_id = request.query_params.get('match_id')
+        if not match_id:
+            return Response({"error": "Falta el parámetro 'match_id'"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            match = Match.objects.get(id=match_id)
+        except Match.DoesNotExist:
+            return Response({"error": f"No existe el partido con id {match_id}"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = MatchSerializer(match)
+        return Response(serializer.data)
+
 
 # --- View to handle requests for the list of teams ---------------------------------------------------------------------------------
 class TeamListView(APIView):
