1366a4f9785f6bd4ed1f522036e2b4a7861aef91
Jaime-Linares <jailinbar@alum.us.es>
2025-04-30 21:22:09 +0200

    feat: add endpoint for last minute for each period of a match

--- backend/matches/real_time_simulation/urls.py
+++ backend/matches/real_time_simulation/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-#from .views import 
+from .views import HalfEndMinutesView
 
 
 urlpatterns = [
-
+    path('match/half_end_minutes/', HalfEndMinutesView.as_view(), name='half_end_minutes'),
 ]
---
+++ backend/matches/real_time_simulation/views.py
@@ -0,0 +1,32 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from matches.models import Match, Event
+
+
+# --- View to handle requests for half end minutes ----------------------------------------------------------------------------------
+class HalfEndMinutesView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        statsbomb_id = request.query_params.get('statsbomb_id')
+        if not statsbomb_id:
+            return Response({"error": "Falta el parámetro 'statsbomb_id'"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            match = Match.objects.get(statsbomb_id=statsbomb_id)
+        except Match.DoesNotExist:
+            return Response({"error": f"No existe el partido con id {statsbomb_id}"}, status=status.HTTP_404_NOT_FOUND)
+
+        events = Event.objects.filter(match=match)
+        first_half = events.filter(period=1).order_by('-minute').first()
+        print(first_half)
+        second_half = events.filter(period=2).order_by('-minute').first()
+        print(second_half)
+
+        return Response({
+            "first_half_final_minute": first_half.minute if first_half else None,
+            "second_half_final_minute": second_half.minute if second_half else None
+        })
+
